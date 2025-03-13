f6847cdd1546738fdb79d90b291a573c0d9ef68e
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-03-14 01:25:24 +0200

    1- Update the name "data" to "requestBody" 2- create a new global variable "studentId" and get its the value from the response. 3- Change the names of the test methods to be more clear and readable 4- add comments to explain why converting JSON object to string when passing to body() function.

--- 02. CreatingPostRequestBody/CreatingPostRequestBody.java
+++ 02. CreatingPostRequestBody/CreatingPostRequestBody.java
@@ -1,23 +1,17 @@
 package day2;
 
-import org.codehaus.groovy.transform.FieldASTTransformation;
-import org.json.JSONTokener;
-import org.testng.annotations.Test;
-
-import com.jayway.jsonpath.JsonPath;
-
-
-
-import org.json.JSONObject;
-
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.HashMap;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
 
 /*
  * Different ways to create POST request body
@@ -29,22 +23,24 @@ import java.util.HashMap;
 
 public class CreatingPostRequestBody {
 
+		String studentId;
+	
 		// 1) Creating post request body using HashMap
-		//@Test
+		// @Test
 		void testPostUsingHashMap()
 		{
-			HashMap data = new HashMap();
+			HashMap<String, Object> requestBody = new HashMap<>();
 			
-			data.put("name", "Abdallah");
-			data.put("location", "Egypt");
-			data.put("phone", "0123456789");
+			requestBody.put("name", "Abdallah");
+			requestBody.put("location", "Egypt");
+			requestBody.put("phone", "0123456789");
 			
 			String coursesArr[] = {"C", "C++"};
-			data.put("courses", coursesArr);
+			requestBody.put("courses", coursesArr);
 			
-			given()
+			studentId = given()
 				.contentType("application/json")
-				.body(data)
+				.body(requestBody)
 			.when()
 				.post("http://localhost:3000/students")
 			.then()
@@ -55,27 +51,28 @@ public class CreatingPostRequestBody {
 				.body("courses[0]", equalTo("C"))
 				.body("courses[1]", equalTo("C++"))
 				.header("Content-Type", "application/json")
-				.log().all();
+				.log().all()
+				.extract().jsonPath().getString("id");
 		}
 		
 		// 2) Creating post request body using Org.JSON library
-		//@Test
-		void testPostUsingJsonLibrary()
+		// @Test
+		void createStudentUsingJsonLibrary()
 		{
-			JSONObject data = new JSONObject();
+			JSONObject requestBody = new JSONObject();
 			
-			data.put("name", "Abdallah");
-			data.put("location", "Egypt");
-			data.put("phone", "0123456789");
+			requestBody.put("name", "Abdallah");
+			requestBody.put("location", "Egypt");
+			requestBody.put("phone", "0123456789");
 			
 			String coursesArr[] = {"C", "C++"};
-			data.put("courses", coursesArr);
+			requestBody.put("courses", coursesArr);
 			
 			
 			
-			given()
+			studentId = given()
 				.contentType("application/json")
-				.body(data.toString())
+				.body(requestBody.toString())	// Convert the request body from JSON Object to String Format
 			.when()
 				.post("http://localhost:3000/students")
 			.then()
@@ -86,56 +83,60 @@ public class CreatingPostRequestBody {
 				.body("courses[0]", equalTo("C"))
 				.body("courses[1]", equalTo("C++"))
 				.header("Content-Type", "application/json")
-				.log().all();
+				.log().all()
+				.extract().jsonPath().getString("id");
 		}
 
 		// 3) Creating post request body using POJO
 		//@Test
-		void testPostUsingPOJO()
+		void createStudentUsingPojoClass()
 		{
-			Pojo_PostRequest data = new Pojo_PostRequest();
+			Pojo_PostRequest requestBody = new Pojo_PostRequest();
 			
-			data.setName("Abdallah");
-			data.setLocation("Egypt");
-			data.setPhone("0123456789");
+			requestBody.setName("Abdallah");
+			requestBody.setLocation("Egypt");
+			requestBody.setPhone("0123456789");
 			
 			String coursesArr[] = {"C", "C++"};
-			data.setCourses(coursesArr);
+			requestBody.setCourses(coursesArr);
 			
 			
 			
 			given()
 				.contentType("application/json")
-				.body(data)
+				.body(requestBody)
 			.when()
 				.post("http://localhost:3000/students")
 			.then()
 				.statusCode(201)
-				.body("name", equalTo("Abdallah"))
-				.body("location", equalTo("Egypt"))
-				.body("phone", equalTo("0123456789"))
-				.body("courses[0]", equalTo("C"))
-				.body("courses[1]", equalTo("C++"))
+				.body("name", equalTo(requestBody.getName()))
+				.body("location", equalTo(requestBody.getLocation()))
+				.body("phone", equalTo(requestBody.getPhone()))
+				.body("courses[0]", equalTo(requestBody.getCourses()[0]))
+				.body("courses[1]", equalTo(requestBody.getCourses()[1]))
 				.header("Content-Type", "application/json")
 				.log().all();
 		}
 		
 		// 4) Creating post request body using External JSON File
-		@Test
-		void testPostUsingExternalJsonFile() throws FileNotFoundException
+		//@Test
+		void createStudentUsingExternalFile() throws FileNotFoundException
 		{
 			
 			File f = new File(".\\body.json");
 			
-			FileReader fr = new FileReader(f);
+			// Read the data from the file
+			FileReader fileReader = new FileReader(f);
 			
-			JSONTokener jt = new JSONTokener(fr);
+			// Text file can be read directly, but JSON files need to be read as tokens
+			JSONTokener jsonTokener = new JSONTokener(fileReader);
 			
-			JSONObject data = new JSONObject(jt);
+			// Convert json tokener into json object
+			JSONObject requestBody = new JSONObject(jsonTokener);
 			
-			given()
+			studentId = given()
 				.contentType("application/json")
-				.body(data.toString())
+				.body(requestBody.toString())	// Convert the JSON object to string format
 			.when()
 				.post("http://localhost:3000/students")
 			.then()
@@ -146,7 +147,20 @@ public class CreatingPostRequestBody {
 				.body("courses[0]", equalTo("C"))
 				.body("courses[1]", equalTo("C++"))
 				.header("Content-Type", "application/json")
-				.log().all();
+				.log().all()
+				.extract().jsonPath().getString("id");
 		}		
+		
+		
+		@AfterMethod
+		void deleteStudentRecord()
+		{
+			given()
+			
+			.when()
+				.delete("http://localhost:3000/students/" + studentId)
+			.then()
+				.statusCode(200);
+		}
 		
 }
