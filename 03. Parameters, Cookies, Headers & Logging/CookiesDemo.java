bc0a69a54e00c7e916e954652e61f71287182fb8
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-03-15 01:20:56 +0200

    1- Modify the Code to be more dynamic 2- extract the response using extract() method 3- Get cookie info Methods[expiration date, value, security]

--- 03. Parameters, Cookies, Headers & Logging/CookiesDemo.java
+++ 03. Parameters, Cookies, Headers & Logging/CookiesDemo.java
@@ -1,14 +1,14 @@
 package day3;
 
-import org.testng.annotations.Test;
-
-import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
 
 import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.Cookie;
+import io.restassured.response.Response;
 
 
 public class CookiesDemo {
@@ -17,17 +17,19 @@ public class CookiesDemo {
 	void testCookies()
 	{
 		
-		given()
+		Response res = given()
 			
 		.when()
 			.get("https://www.google.com/")
 		.then()
-			// The cookie value changes with every request
-			// So, if the test failed, it's working fine.
-			.cookie("AEC", "AVcja2c26HL_hzcenmaIB2C3jUl13zVzLWdlu9-1lYoA8Kj3iOB9ckvWBg")
-			.log().all();
+			.log().cookies()
+			.statusCode(200)
+			.cookie("AEC", notNullValue())
+			.extract().response();
 		
-		
+		// Extract a specific cookie
+		String cookieValue = res.getCookie("AEC");
+		System.out.println("Value of 'AEC' Cookie: " + cookieValue);
 		
 	}
 	
@@ -55,6 +57,22 @@ public class CookiesDemo {
 			String current_cookie_value = res.getCookie(key);
 			System.out.println(key + "     " + current_cookie_value);
 		}
+		
+		
+		// Get detailed information about cookie
+		Cookie cookie_info = res.getDetailedCookie("AEC");
+		
+		// Verify the cookie expiration date
+		System.out.println(cookie_info.hasExpiryDate());
+		System.out.println(cookie_info.getExpiryDate());
+		
+		// Verify the cookie value
+		System.out.println(cookie_info.hasValue());
+		System.out.println(cookie_info.getValue());
+		
+		// Verify the cookie security
+		System.out.println(cookie_info.isSecured());
+		
 	}
-	
+	 
 }
